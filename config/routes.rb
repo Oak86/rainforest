f6dc71d240010301bebeb91da914166d748c74a4
oak86 <email@omarkhafagy.com>
2013-09-11 18:08:11 -0400

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rainforest::Application.routes.draw do
+  get "users/new"
+
+  get "users/create"
+
   root :to => "products#index"
-  
+
   resources :products
+  resources :users, :only => [:new, :create]
 end
